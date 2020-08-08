e84f569ca55cbff0c01990931901dca01bf993be
Sorawit Stapornchaisit <46063093+kidserge-yong@users.noreply.github.com>
2020-08-08 20:49:28 +0900

    changes h() to sqrt((x1 - x2)**2 + (y1 - y2)**2)

--- gui_pathfinder.py
+++ gui_pathfinder.py
@@ -114,7 +114,7 @@ class Spot:
 def h(p1, p2):
 	x1, y1 = p1
 	x2, y2 = p2
-	return abs(x1 - x2) + abs(y1 - y2)
+	return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 
 def reconstruct_path(came_from, current, draw):
